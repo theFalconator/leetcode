e0fcc5a34c09eed651927ed9644ece8e03d299c3
theFalconator <falcon.nick@gmail.com>
2017-09-02 10:41:48 -0500

    refactored tests

--- PalindromeNumber/test/test_solution.py
+++ PalindromeNumber/test/test_solution.py
@@ -1,14 +1,27 @@
 from unittest import TestCase
+
 from PalindromeNumber.solution import Solution
 
 
-class TestSolution(TestCase):
-    def test_splitNumber(self):
+class TestSplit(TestCase):
+    def test_even_number_of_digits(self):
         s = Solution()
         self.assertEqual(s.splitInt(1221), [1, 2, 2, 1])
-        self.assertEqual(s.splitInt(150), [])
-        self.assertEqual(s.splitInt(121), [1,2,1])
 
+    def test_ends_in_zero(self):
+        s = Solution()
+        self.assertEqual(s.splitInt(150), [1, 5, 0])
+
+    def test_odd_number_of_digits(self):
+        s = Solution()
+        self.assertEqual(s.splitInt(121), [1, 2, 1])
+
+    def test_intersperced_zeros(self):
+        s = Solution()
+        self.assertEqual(s.splitInt(1000030001), [1, 0, 0, 0, 0, 3, 0, 0, 0, 1])
+
+
+class TestPalindrome(TestCase):
     def test_ends_in_zero(self):
         s = Solution()
         self.assertEqual(s.isPalindrome(150), False)
@@ -28,3 +41,7 @@ class TestSolution(TestCase):
     def test_even_number_of_digits(self):
         s = Solution()
         self.assertEqual(s.isPalindrome(1221), True)
+
+    def test_intersperced_zeros(self):
+        s = Solution()
+        self.assertEqual(s.isPalindrome(1000030001), False)
