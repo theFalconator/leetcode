0af344d6fcad7b94cfcfa5ed8a95e115f2563309
theFalconator <falcon.nick@gmail.com>
2017-09-02 16:22:28 -0500

    working solution

--- RomanToInteger/solution.py
+++ RomanToInteger/solution.py
@@ -5,3 +5,32 @@ class Solution:
         :param s: Input string should be a roman numeral ranging from 1 to 3999.
         :return: An integer representation of the input.
         '''
+        roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        numerals = list(roman_numerals.keys())
+
+        if len(s) == 1:
+            return roman_numerals.get(s, None)
+        else:
+            total = 0
+            for i in range(0, len(s)):
+                # Roman numerals are always written from left to right in order of value.
+                if i < len(s) - 1:
+                    # look ahead to see if we need to add or subtract
+                    current = s[i]
+                    next = s[i + 1]
+                    if numerals.index(current) < numerals.index(next):
+                        total -= roman_numerals.get(s[i], 0)
+                    else:
+                        total += roman_numerals.get(s[i], 0)
+                else:
+                    total += roman_numerals.get(s[i], 0)
+            return total
--- RomanToInteger/test/test_solution.py
+++ RomanToInteger/test/test_solution.py
@@ -1,6 +1,60 @@
 from unittest import TestCase
 
+from RomanToInteger.solution import Solution
+
+
+class TestDict(TestCase):
+    def test_what_order_are_keys(self):
+        # do keys come out of the dictionary the same order I put them in there?
+        roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        self.assertEqual(list(roman_numerals.keys()), ['I', 'V', 'X', 'L', 'C', 'D', 'M'])
 
 class TestSolution(TestCase):
-    def test_romanToInt(self):
-        self.fail()
+    def test_individual_numerals(self):
+        known = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        s = Solution()
+        for numeral in known:
+            self.assertEqual(s.romanToInt(numeral), known[numeral])
+
+    def test_multiple_numerals(self):
+        s = Solution()
+        self.assertEqual(s.romanToInt('II'), 2)
+        self.assertEqual(s.romanToInt('DCXXI'), 621)
+
+    def test_descending_order(self):
+        s = Solution()
+        self.assertEqual(s.romanToInt('MDC'), 1600)
+        self.assertEqual(s.romanToInt('LX'), 60)
+        self.assertEqual(s.romanToInt('VIII'), 8)
+
+    def test_subtractive_notation(self):
+        s = Solution()
+        known = {
+            'IV': 4,
+            'IX': 9,
+            'XC': 90,
+            'CD': 400,
+            'CM': 900
+        }
+        for numeral in known:
+            self.assertEqual(s.romanToInt(numeral), known[numeral])
+
+    def test_max_value(self):
+        s = Solution()
+        self.assertEqual(s.romanToInt('MMMCMXCIX'), 3999)
