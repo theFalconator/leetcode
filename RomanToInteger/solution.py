2fe114ea9f18dc6ba4ac33dc43f2f6468b4879f2
theFalconator <falcon.nick@gmail.com>
2017-09-02 15:50:07 -0500

    added skeleton for RomanToInteger

---
+++ RomanToInteger/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def romanToInt(self, s):
+        '''
+        Converts roman numeral string to integer.
+        :param s: Input string should be a roman numeral ranging from 1 to 3999.
+        :return: An integer representation of the input.
+        '''
