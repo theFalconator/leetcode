c1bc12030003799ca410f0ef56a766b3c57282f7
theFalconator <falcon.nick@gmail.com>
2017-09-02 10:34:32 -0500

    added test cases

--- PalindromeNumber/solution.py
+++ PalindromeNumber/solution.py
@@ -1,22 +1,45 @@
 import math
 
+
 class Solution:
     def splitInt(self, x):
         l = []
         while x % 10 != 0:
             l.append(x % 10)
-            x = math.floor(x/10)
+            x = math.floor(x / 10)
         return l
 
     def isPalindrome(self, x):
+        # negative numbers cannot be palindromes
         if x < 0:
             return False
+
+        # single digit numbers can.
+        if x > 0 and x < 10:
+            return True
+
+        # numbers ending in zero cannot.
         if x % 10 == 0 and x != 0:
             return False
         else:
             l = self.splitInt(x)
-            first_half = l[0:math.floor(len(l)/2)]
-            second_half = l[math.floor(len(l)/2):len(l)]
-            if first_half[::-1] == second_half:
-                return True
-            return False
+
+            # even number of digits
+            if len(l) % 2 == 0:
+                i = math.floor(len(l) / 2)
+                j = math.floor(len(l) / 2)
+                first_half = l[0:i]
+                second_half = l[j:len(l)]
+                if first_half[::-1] == second_half:
+                    return True
+                return False
+
+            # odd number of digits
+            else:
+                i = math.floor(len(l) / 2)
+
+                first_half = l[0:i]
+                second_half = l[i + 1:len(l)]
+                if first_half[::-1] == second_half:
+                    return True
+                return False
--- PalindromeNumber/test/test_solution.py
+++ PalindromeNumber/test/test_solution.py
@@ -7,8 +7,24 @@ class TestSolution(TestCase):
         s = Solution()
         self.assertEqual(s.splitInt(1221), [1, 2, 2, 1])
         self.assertEqual(s.splitInt(150), [])
+        self.assertEqual(s.splitInt(121), [1,2,1])
 
-    def test_isPalindrome(self):
+    def test_ends_in_zero(self):
+        s = Solution()
+        self.assertEqual(s.isPalindrome(150), False)
+
+    def test_negative_number(self):
+        s = Solution()
+        self.assertEqual(s.isPalindrome(-5), False)
+
+    def test_zero(self):
+        s = Solution()
+        self.assertEqual(s.isPalindrome(0), True)
+
+    def test_odd_number_of_digits(self):
+        s = Solution()
+        self.assertEqual(s.isPalindrome(121), True)
+
+    def test_even_number_of_digits(self):
         s = Solution()
         self.assertEqual(s.isPalindrome(1221), True)
-        self.assertEqual(s.isPalindrome(150), False)
