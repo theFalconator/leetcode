53de81f7f40592f3e7849b5df7854dcd4d3886f9
theFalconator <falcon.nick@gmail.com>
2017-09-02 22:38:15 -0500

    LongestCommonPrefix skeleton

---
+++ LongestCommonPrefix/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        '''
+        Given a list of strings return the longest common prefix.
+        :param strs: List of strings to search for prefix
+        :return: string containing the longest common prefix
+        '''
+        pass
---
+++ LongestCommonPrefix/test/test_solution.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from LongestCommonPrefix.solution import Solution
+
+
+class TestSolution(TestCase):
+    def test_longestCommonPrefix(self):
+        s = Solution()
+        self.fail()
