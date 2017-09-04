f2fc91485cad835442a48e559025509ace5cf6f8
theFalconator <falcon.nick@gmail.com>
2017-09-04 11:52:18 -0500

    MergeTwoLists - recursive solution

--- MergeTwoSortedLists/solution.py
+++ MergeTwoSortedLists/solution.py
@@ -1,16 +1,18 @@
 class ListNode:
-    def __init__(self, x=0):
+    def __init__(self, x):
         self.val = x
         self.next = None
 
-    def __repr__(self):
-        nodes = []
-        current = self
-        while current is not None:
-            nodes.append(str(current.val))
-            current = current.next
+    def __eq__(self, other):
+        return self.val == other.val and self.next == other.next
 
-        return '-->'.join(nodes)
+
+def connect_nodes(lst):
+    prev = lst[0]
+    for node in lst[1:]:
+        prev.next = node
+        prev = node
+    return lst[0]
 
 
 class Solution:
@@ -21,3 +23,20 @@ class Solution:
         :param l2: ListNode
         :return: ListNode
         '''
+
+        if l1 is None and l2 is None:
+            return None
+
+        if l1 is None:
+            return l2
+
+        if l2 is None:
+            return l1
+
+        if l1.val <= l2.val:
+            min_node = l1
+            min_node.next = self.mergeTwoLists(l1.next, l2)
+        elif l1.val > l2.val:
+            min_node = l2
+            min_node.next = self.mergeTwoLists(l1, l2.next)
+        return min_node
--- MergeTwoSortedLists/test/test_solution.py
+++ MergeTwoSortedLists/test/test_solution.py
@@ -1,6 +1,25 @@
 from unittest import TestCase
 
+from MergeTwoSortedLists.solution import ListNode, Solution
+
 
 class TestSolution(TestCase):
-    def test_mergeTwoLists(self):
-        self.fail()
+    def test_two_empty_lists(self):
+        s = Solution()
+        self.assertEqual(s.mergeTwoLists(None, None), None)
+
+    def test_one_list_empty(self):
+        s = Solution()
+        self.assertEqual(s.mergeTwoLists(None, ListNode(1)), ListNode(1))
+        self.assertEqual(s.mergeTwoLists(ListNode(1), None), ListNode(1))
+
+    def test_insert_at_front(self):
+        s = Solution()
+        l1 = ListNode(1)
+        l2 = ListNode(2)
+        l2.next = ListNode(3)
+
+        merged = ListNode(1)
+        merged.next = ListNode(2)
+        merged.next.next = ListNode(3)
+        self.assertEqual(s.mergeTwoLists(l1, l2), merged)
